c0750e9b956870d36ba5e57dbc329b47a68dfa09
qvd808 <dqvinh808@gmail.com>
2023-02-23 13:53:54 -0800

    Finished glitch text for Welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,121 @@
-import { useState } from 'react'
+import { motion, useCycle } from "framer-motion";
+import { useEffect, useState } from "react";
+import GlitchText from "./GlitchText";
+
+const glitch = {
+  textEffect: {
+    textShadow: [
+      "0.05em 0 0 rgba(255, 0, 0, 0.75), -0.025em -0.05em 0 rgba(0, 255, 0, 0.75),-0.025em 0.05em 0 rgba(0, 0, 255, 0.75)",
+      "0.05em 0 0 rgba(255, 0, 0, 0.75),-0.05em -0.025em 0 rgba(0, 255, 0, 0.75),-0.025em 0.05em 0 rgba(0, 0, 255, 0.75)",
+      "-0.05em -0.025 0 rgba(255, 0, 0, 0.75),0.025em 0.025em 0 rgba(0, 255, 0, 0.75),-0.05em -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "-0.05em -0.025 0 rgba(255, 0, 0, 0.75),0.025em 0.025em 0 rgba(0, 255, 0, 0.75),-0.05em -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "0.025em 0.05 0 rgba(255, 0, 0, 0.75),0.05em 0 0 rgba(0, 255, 0, 0.75),0 -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "0.025em 0.05 0 rgba(255, 0, 0, 0.75),0.05em 0 0 rgba(0, 255, 0, 0.75),0 -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "-0.025em 0 0 rgba(255, 0, 0, 0.75),-0.025em -0.025em 0 rgba(0, 255, 0, 0.75),-0.025em -0.05em 0 rgba(0, 0, 255, 0.75)",
+    ],
+    opacity: [1, 1, 1, 1, 1, 1, 0],
+  },
+
+  textEffectTranslateDown: {
+    textShadow: [
+      "0.05em 0 0 rgba(255, 0, 0, 0.75), -0.025em -0.05em 0 rgba(0, 255, 0, 0.75),-0.025em 0.05em 0 rgba(0, 0, 255, 0.75)",
+      "0.05em 0 0 rgba(255, 0, 0, 0.75),-0.05em -0.025em 0 rgba(0, 255, 0, 0.75),-0.025em 0.05em 0 rgba(0, 0, 255, 0.75)",
+      "-0.05em -0.025 0 rgba(255, 0, 0, 0.75),0.025em 0.025em 0 rgba(0, 255, 0, 0.75),-0.05em -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "-0.05em -0.025 0 rgba(255, 0, 0, 0.75),0.025em 0.025em 0 rgba(0, 255, 0, 0.75),-0.05em -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "0.025em 0.05 0 rgba(255, 0, 0, 0.75),0.05em 0 0 rgba(0, 255, 0, 0.75),0 -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "0.025em 0.05 0 rgba(255, 0, 0, 0.75),0.05em 0 0 rgba(0, 255, 0, 0.75),0 -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "-0.025em 0 0 rgba(255, 0, 0, 0.75),-0.025em -0.025em 0 rgba(0, 255, 0, 0.75),-0.025em -0.05em 0 rgba(0, 0, 255, 0.75)",
+    ],
+    transform: [
+      "translate(0.025em, 0.05em)",
+      "translate(-0.025em, -0.0125em)",
+      "translate(-0.025em, -0.0125em)",
+      "translate(0.04em, 0.1em)",
+      "translate(-0.025em, -0.0125em)",
+      "translate(-0.025em, -0.0125em)",
+      "translate(0.025em, 0.05em)",
+    ],
+    opacity: [0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0],
+  },
+  textEffectTranslateUp: {
+    textShadow: [
+      "0.05em 0 0 rgba(255, 0, 0, 0.75), -0.025em -0.05em 0 rgba(0, 255, 0, 0.75),-0.025em 0.05em 0 rgba(0, 0, 255, 0.75)",
+      "0.05em 0 0 rgba(255, 0, 0, 0.75),-0.05em -0.025em 0 rgba(0, 255, 0, 0.75),-0.025em 0.05em 0 rgba(0, 0, 255, 0.75)",
+      "-0.05em -0.025 0 rgba(255, 0, 0, 0.75),0.025em 0.025em 0 rgba(0, 255, 0, 0.75),-0.05em -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "-0.05em -0.025 0 rgba(255, 0, 0, 0.75),0.025em 0.025em 0 rgba(0, 255, 0, 0.75),-0.05em -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "0.025em 0.05 0 rgba(255, 0, 0, 0.75),0.05em 0 0 rgba(0, 255, 0, 0.75),0 -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "0.025em 0.05 0 rgba(255, 0, 0, 0.75),0.05em 0 0 rgba(0, 255, 0, 0.75),0 -0.05em 0 rgba(0, 0, 255, 0.75)",
+      "-0.025em 0 0 rgba(255, 0, 0, 0.75),-0.025em -0.025em 0 rgba(0, 255, 0, 0.75),-0.025em -0.05em 0 rgba(0, 0, 255, 0.75)",
+    ],
+    transform: [
+      "translate(0.025em, 0.05em)",
+      "translate(0.015em, 0.025em)",
+      "translate(0.015em, 0.025em)",
+      "translate(-0.04em, -0.1em)",
+      "translate(0.015em, 0.025em)",
+      "translate(0.015em, 0.025em)",
+      "translate(0.025em, 0.05em)",
+    ],
+    opacity: [0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0],
+  },
+};
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-      
+      <motion.div>
+        
+      </motion.div>
+      <motion.div
+        className="glitch"
+        variants={glitch}
+        animate="textEffect"
+        transition={{
+          duration: 0.5,
+          times: [0, 0.14, 0.15, 0.49, 0.5, 0.99, 1],
+        }}
+      >
+        Welcome
+      </motion.div>
+      <motion.div
+        className="glitch"
+        style={{
+          position: "absolute",
+          clipPath: "polygon(0 0, 100% 0, 100% 45%, 0 45%)",
+          transform: "translate(-0.025em, -0.0125em)",
+          opacity: 0.8,
+        }}
+        variants={glitch}
+        animate="textEffectTranslateUp"
+        transition={{
+          duration: 0.65,
+          times: [0, 0.14, 0.15, 0.49, 0.5, 0.99, 1],
+        }}
+      >
+        Welcome
+      </motion.div>
+      <motion.div
+        className="glitch"
+        style={{
+          position: "absolute",
+          clipPath: "polygon(0 80%, 100% 20%, 100% 100%, 0 100%)",
+          transform: "translate(0.015em, 0.025em)",
+          opacity: 0.8,
+        }}
+        variants={glitch}
+        animate="textEffectTranslateDown"
+        transition={{
+          duration: 0.375,
+          times: [0, 0.14, 0.15, 0.49, 0.5, 0.99, 1],
+        }}
+      >
+        Welcome
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
