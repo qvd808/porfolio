35e5495444a4b759006b20b3c3b4d7875be58477
qvd808 <dqvinh808@gmail.com>
2023-02-24 16:39:48 -0800

    Making the screen flexible

--- src/component/pages/ProjectContent.jsx
+++ src/component/pages/ProjectContent.jsx
@@ -1,29 +1,31 @@
 import { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
 
-export default function ProjectContent ({name}) {
-
-const [projectData, setProjectData] = useState("");
+export default function ProjectContent({ name }) {
+  const ProjectName = name.split(".md")[0];
+  const [projectData, setProjectData] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       const result = await axios.get(
-        "https://raw.githubusercontent.com/qvd808/Porfolio-site-content/main/DogCatRecognition.md"
+        `https://raw.githubusercontent.com/qvd808/Porfolio-site-content/main/${name}`
       );
       setProjectData(result.data);
     }
     fetchData();
   }, []);
 
-
-    return (
-        <div className="project-card">
-            {name}
-            <div>Desscription
-                <div>
-                    {projectData}
-                </div>
-            </div>
-        </div>
-    )
-} 
+  return (
+    <div className="project-card">
+      <div className="project-title"
+      >
+        {ProjectName}
+      </div>
+      <div className="project-card-content">
+        Desscription
+        <ReactMarkdown>{projectData}</ReactMarkdown>
+      </div>
+    </div>
+  );
+}
