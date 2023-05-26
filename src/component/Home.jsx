71e1b9d09b030e665820f9c97639a5cd96540d72
Quang Vinh Dang <dqvinh808@gmail.com>
2023-05-26 16:52:15 -0700

    Update syntax

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,25 +1,25 @@
 import { motion, useAnimation, AnimatePresence } from "framer-motion";
 import { Link, BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import AboutMe from "./pages/Aboutme";
+import AboutMe from "./pages/AboutMe";
 import NavBar from "./NavBar";
 import Projects from "./pages/Projects";
 import MainPage from "./pages/MainPage";
 
 export default function Home() {
 
-    const location = useLocation();
+  const location = useLocation();
 
   return (
     <>
       <NavBar />
 
       {/* <div className="flex flex-col items-center min-h-screen bg-gray-900 text-white"> */}
-        <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route exact path="/qvd-portfolio" element={<AboutMe />} />
-            <Route  exact path="/qvd-portfolio/projects" element={<Projects />} />
-          </Routes>
-        </AnimatePresence>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route exact path="/qvd-portfolio" element={<AboutMe />} />
+          <Route exact path="/qvd-portfolio/projects" element={<Projects />} />
+        </Routes>
+      </AnimatePresence>
       {/* </div> */}
 
     </>
