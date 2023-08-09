9f97f596305cd4f8565906cb54b70aecb3818425
Quang Vinh Dang <dqvinh808@gmail.com>
2023-08-09 09:39:29 -0700

    Update base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://qvd808.github.io/qvd-portfolio"
+  base: "https://qvd808.github.io/Porfolio"
 })
