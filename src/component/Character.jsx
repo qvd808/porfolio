eba3c04184ba3d01d36839189784a94bdc38422f
Quang Vinh Dang <dqvinh808@gmail.com>
2024-02-04 23:24:32 -0800

    Adding robot

--- src/component/Character.jsx
+++ src/component/Character.jsx
@@ -1,31 +1,30 @@
-import Spline from '@splinetool/react-spline';
-import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+import { useState } from "react";
+import Robot from "./animation/robot.json";
 
 const animationEffect = {
-    initial: { opacity: 0, y: "30em" },
-    animate: {
-        opacity: 1, y: 0, transition: {
-            type: "spring",
-            duration: 2.5,
-        }
+  initial: { opacity: 0, y: "30em" },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      duration: 2.5,
     },
-    exit: {
-        opacity: 0, y: 50
-    }
-}
-
+  },
+  exit: {
+    opacity: 0,
+    y: 50,
+  },
+};
 
 export default function Character() {
-    return (
-        // <div className="w-[30vw] h-[30vh] my-20"
-            // key="character"
-            // variants={animationEffect}
-            // initial="initial"
-            // animate="animate"
-            // exit="exit"
-        // >
-            <Spline scene="https://prod.spline.design/XzwQUW7aWZMLQFKo/scene.splinecode"/>
-        // </div>
-    )
+  return (
+    <div>
+     <Lottie 
+    autoplay
+    loop={false}
+     animationData={Robot} />
+    </div>
+  );
 }
