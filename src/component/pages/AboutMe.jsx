4d2614924347462df059efe69c301abadacbc80b
Quang Vinh Dang <dqvinh808@gmail.com>
2024-02-06 23:12:21 -0800

    Adding chatbot

--- src/component/pages/AboutMe.jsx
+++ src/component/pages/AboutMe.jsx
@@ -1,17 +1,23 @@
-import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useState } from "react";
-
+import { AnimatePresence, motion, useReducedMotion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { chatBotAPI } from "../../api/api";
 import SkillLoadingEffect from "../SkillLoadingEffect";
 import Introduction from "../Introduction";
 import Contact from "../Contact";
 import Character from "../Character";
+import Waiting from "../animation/waiting.json";
+import Lottie from "lottie-react";
+import BoldText from "../../textEffect/BoldText";
 
 export default function AboutMe() {
   const [isMouseDown, setIsMouseDown] = useState(false);
   const [isSkillDisplayed, setIsSkillDisplayed] = useState(false);
   const [isIntroductionDisplayed, setIsIntroductionDisplayed] = useState(false);
   const [isContactDisplayed, setIsContactDisplayed] = useState(false);
-  const [botMessage, setBotMessage] = useState("Hello, there! ٩(^ᗜ^ )و ´-");
+  const [botMessage, setBotMessage] = useState("Hello there! ٩(^ᗜ^ )و My name is Botley. I am a chatbot created by Vinh. You can ask me anything about Vinh, his skills, and his experience. I will try my best to answer your questions. (´･ω･`)");
+  // const [botMessage, setBotMessage] = useState(
+  //   "My creator is Dang Quang Vinh. He is a skilled software engineer with experience in various technologies such as React, NextJs, NodeJs, Express, AWS, Java, C, bash, Python, Pytorch, and more. He has worked on projects such as VIRENTO, an online renting platform, and Forget Me Not, a mobile application for caregivers and dementia patients. He has also worked on personal projects like writing an operating system from scratch and creating a Flappy Bird bot. He has experience as an IT Associate/Junior Software Engineer at Atimi Software, where he utilized technologies like React Native, Python, Power Automate, Firebase, and more."
+  // );
 
   const parentVariants = {
     initial: {},
@@ -32,6 +38,32 @@ export default function AboutMe() {
       opacity: 0,
       y: 50,
     },
+  };
+
+  const getResponse = async () => {
+    setBotMessage("");
+
+    const response = await fetch(`${chatBotAPI}/ask`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        question: document.getElementById("chatbox").value,
+      }),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        return data.answer;
+      })
+      .catch((err) => {
+        console.log(err);
+        return "There might be problem within the server. Sorry for the inconvenience. (´-ω-`)";
+      });
+    document.getElementById("chatbox").value = "";
+    setBotMessage(response);
   };
 
   return (
@@ -118,7 +150,7 @@ export default function AboutMe() {
           !isIntroductionDisplayed && (
             <motion.div
               key="robot"
-              className="relative flex justify-center flex-col items-center w-full max-w-2xl mx-auto p-4"
+              className="flex justify-center flex-col items-center w-full max-w-6xl mx-auto p-4 md:flex-row-reverse"
               initial={{
                 opacity: 0,
                 y: "30vh",
@@ -136,10 +168,38 @@ export default function AboutMe() {
                 y: "5vh",
               }}
             >
-              <div className="relative top-16 p-3 bg-stone-100 w-full h-44 clip-message text-black">
-                {botMessage}
+              <div className="relative p-2 bg-stone-100 w-full h-auto text-black rounded-lg md:text-2xl lg:left-10 lg:top-16">
+                <div className="flex mb-5 mx-3 mt-3 justify-center items-center text-center md:min-h-[300px]">
+                  {botMessage.length !== 0 ? (
+                    <BoldText text={botMessage} />
+                  ) : (
+                    <Lottie
+                      key="waiting"
+                      className="w-2/5 bottom-4 left-2"
+                      animationData={Waiting}
+                      autoplay
+                      loop={true}
+                    />
+                  )}
+                </div>
               </div>
-              <Character />
+              <div className="mx-5">
+                <Character />
+                <div class="grid grid-cols-[90%_10%] mx-3 my-12">
+                  <input
+                    class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="chatbox"
+                    type="text"
+                    placeholder="Ask me about Vinh!!"
+                  ></input>
+                  <button className="bg-white rounded-md" onClick={getResponse}>
+                    <img
+                      className="w-auto h-auto object-contain"
+                      src="./send.svg"
+                    />
+                  </button>
+                </div>
+              </div>
             </motion.div>
           )}
 
