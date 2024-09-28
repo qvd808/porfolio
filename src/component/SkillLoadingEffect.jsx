680504860a28a8f5dc8516bca7a979e344016031
Quang Vinh Dang <dqvinh101@gmail.com>
2024-09-27 22:58:26 -0700

    Adding wheel text

--- src/component/SkillLoadingEffect.jsx
+++ src/component/SkillLoadingEffect.jsx
@@ -5,7 +5,12 @@ export default function SkillLoadingEffect({ onMouseDown }) {
 	const [sectorAngle, setSectorAngle] = useState(0);
 	const openingDuration = 1;
 	const spinDuration = 0;
-
+	const sector_text = [
+		"Development Skill",
+		"Programming Language",
+		"Coming soon",
+		"Exit",
+	];
 	const animationEffect = {
 		initial: { opacity: 0, y: "100vh" },
 		animate: {
@@ -19,9 +24,9 @@ export default function SkillLoadingEffect({ onMouseDown }) {
 		},
 		exit: { opacity: 0, y: 50 },
 	};
-
 	const handleSectorClick = (event, sectorNumber) => {
-		event.stopPropagation(); // Prevent event from bubbling up
+		console.log(sectorNumber);
+		event.stopPropagation();
 		switch (sectorNumber) {
 			case 1:
 				setSectorAngle(270);
@@ -32,17 +37,16 @@ export default function SkillLoadingEffect({ onMouseDown }) {
 			case 3:
 				setSectorAngle(90);
 				break;
-			default:
-				setSectorAngle(0);
+			case 4:
+				setSectorAngle(360);
 				break;
 		}
 	};
-
 	const sectors = [
-		{ color: "#FF6B6B", angle: 90 },
-		{ color: "#4ECB71", angle: 90 },
 		{ color: "#FEC771", angle: 90 },
+		{ color: "#4ECB71", angle: 90 },
 		{ color: "#4A9FF5", angle: 90 },
+		{ color: "#FF6B6B", angle: 90 },
 	];
 
 	return (
@@ -67,25 +71,36 @@ export default function SkillLoadingEffect({ onMouseDown }) {
 					duration: spinDuration,
 				}}
 				onAnimationComplete={() => {
-					if (sectorAngle === 270) {
+					if (sectorAngle === 360) {
 						onMouseDown();
 					}
 				}}
 			>
 				<div className="relative w-full h-full">
 					{sectors.map((sector, index) => {
-						const rotation = index * sector.angle; // Calculate rotation for each sector
+						const rotation = index * sector.angle;
 						return (
 							<div
 								key={index}
 								className="absolute w-full h-full"
 								style={{
 									backgroundColor: sector.color,
-									clipPath: `polygon(50% 50%, 100% 0%, 100% 100%)`, // Adjust for each sector
+									clipPath: `polygon(50% 50%, 100% 0%, 100% 100%)`,
 									transform: `rotate(${rotation}deg)`,
 								}}
 								onClick={(e) => handleSectorClick(e, index + 1)}
-							/>
+							>
+								<div
+									className="absolute top-0 right-0 w-1/2 h-full flex items-center justify-center"
+									style={{
+										transform: `rotate(-${sectorAngle + 90 * index - 90}deg)`,
+									}}
+								>
+									<p className="text-black text-sm font-bold text-center px-2 transform -rotate-90">
+										{sector_text[index]}
+									</p>
+								</div>
+							</div>
 						);
 					})}
 				</div>
